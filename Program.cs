b0da5658cd4453a7e603d683ff965231f8b074ee
Majulia <majulialcarvalho@gmail.com>
2022-03-14 11:15:31 -0300

    A simple text editor

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
