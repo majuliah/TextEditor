292ea57ebb57bd9785cf76f0befdd9972dc123f9
Majulia <majulialcarvalho@gmail.com>
2022-03-14 11:40:56 -0300

    Adding menu options

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            
+        }
+
+        static void Menu()
+        {
+            Clear();
+            WriteLine($"Hey! What do you want to do? ");
+            WriteLine($"*** [ 1 ] Open file       ***");
+            WriteLine($"*** [ 2 ] Edit file       ***");
+            WriteLine($"*** [ 0 ] Close           ***");
+
+            short option = short.Parse(ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Open()
+        {
+            
+        }
+
+        static void Edit()
+        {
+            
         }
     }
 }
